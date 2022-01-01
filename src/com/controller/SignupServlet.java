a15e20a0ff5042272a1acab6a95a5fc3f5ddd726
Tejas <tejas14all@gmail.com>
2022-01-01 16:56:08 +0530

    read parameter

--- src/com/controller/SignupServlet.java
+++ src/com/controller/SignupServlet.java
@@ -1,7 +1,41 @@
 package com.controller;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class SignupServlet  extends HttpServlet{
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignupServlet extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		System.out.println("SignupServlet::service()");
+
+		String firstName = request.getParameter("firstName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		System.out.println(firstName);
+		System.out.println(email);
+		System.out.println(password);
+
+		// produce html content using servlet -->
+
+		response.setContentType("text/html");// MIME
+
+		PrintWriter out = response.getWriter();
+
+		out.print("<html><body>");
+		out.print("FirstName : " + firstName + "<br>");
+		out.print("Email : " + email + "<br>");
+		out.print("Password : " + password + "<br>");
+
+		out.print("</body></html>");
+
+	}
 
 }
